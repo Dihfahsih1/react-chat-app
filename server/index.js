20de74760959eb5567e77bf02d66f1b833e2752f
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-12 02:47:19 +0300

    refreshing browser deletes prev messages

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ io.on('connection', (socket) =>{
   socket.on('Join', ({name, room}, callback) =>{
     const { error, user} = addUser({ id: socket.id, name, room });
     if(error) return callback(error);
-
     socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+
     socket.broadcast.to(user.room).emit('message',{user: 'admin', text: `${user.name}, has joined!`})
 
     socket.join(user.room);
@@ -26,7 +26,6 @@ io.on('connection', (socket) =>{
   });
 
   socket.on('disconnect', () =>{
-    console.log('User had left!!!');
   })
 
 });
