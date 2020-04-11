620e38699e7a8b802fa5bbcc3b84d2878fcae066
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-11 18:02:13 +0300

    change1

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
 io.on('connection', (socket) =>{
   socket.on('Join', ({name, room}, callback) =>{
     const { error, user} = addUser({ id: socket.id, name, room });
@@ -20,11 +19,10 @@ io.on('connection', (socket) =>{
     socket.broadcast.to(user.room).emit('message',{user: 'admin', text: `${user.name}, has joined!`})
 
     socket.join(user.room);
-    callback(); 
+    callback();
   });
   socket.on('sendMessage', (message, callback) =>{
     const user = getUser(socket.id);
-
     io.to(user.room).emit('message', { user: user.name, text: message});
     callback();
   });
