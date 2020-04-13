50bc5dd545b450b1a76d04419f691dda4159bc73
Dihfahsih1 <dihfahsihm@gmail.com>
2020-04-13 11:27:27 +0300

    changes

--- server/users.js
+++ server/users.js
@@ -3,7 +3,9 @@ const users = [];
 const addUser = ({id, name, room }) =>{
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
+
   const existingUser = users.find((user) => user.room === room && user.name === name);
+  
   if(existingUser){
     return{error : 'Username is taken'};
   }
